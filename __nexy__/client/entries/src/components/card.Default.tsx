f932f243dae550630c0268e2f60b72cbb8fd68ce
hopcy-forcy <hopcy.forcy@gmail.com>
2026-04-23 16:08:53 +0100

    Add new font files and components for UI enhancements
    - Added multiple font files for the Inter typeface in various styles (ExtraBoldItalic, ExtraLight, ExtraLightItalic, Italic, Light, LightItalic, Medium, MediumItalic, Regular, SemiBold, SemiBoldItalic, Thin, ThinItalic).
    - Created a CSS file to define font-face rules for 'Geist' and 'Inter' fonts.
    - Introduced new SVG icons for UI elements (luilka.svg, nexy.svg).
    - Added snippets for various UI components including theme toggles, search buttons, language display, and code blocks.
    - Implemented Counter and Card components with responsive design and hover effects.
    - Enhanced the overall UI with new styles and transitions for better user experience.

--- __nexy__/client/entries/src/components/card.Default.tsx
+++
@@ -1,6 +0,0 @@
-import { hydrateRoot } from 'react-dom/client'
-import React from 'react'
-import Component from '../../../../../src/components/card'
-
-const el = document.getElementById('card.Default-root')
-if (el) hydrateRoot(el, React.createElement(Component))
