ea6d5e4ce48a53e52bc86ea265c90129823b3f93
hopcy-forcy <hopcy.forcy@gmail.com>
2026-04-25 02:55:50 +0100

    Refactor code structure for improved readability and maintainability

--- __nexy__/src/routes/index.py
+++ __nexy__/src/routes/index.py
@@ -7,12 +7,13 @@ NexyElement = Union[callable, __JinjaTemplate]
 
 def Index() -> str:
         from __nexy__.src.components.sections.home.ContentBar import ContentBar
+    from __nexy__.src.components.sections.home.HeroSection import HeroSection
     from __nexy__.src.components.sections.home.BaseSection import BaseSection
     from __nexy__.src.components.sections.home.WhatSection import WhatSection
     from __nexy__.src.components.sections.home.WhySection import WhySection
     from __nexy__.src.components.sections.home.IntegrationSection import IntegrationSection
     
-    context = {"BaseSection": BaseSection, "ContentBar": ContentBar, "IntegrationSection": IntegrationSection, "WhatSection": WhatSection, "WhySection": WhySection}
+    context = {"BaseSection": BaseSection, "ContentBar": ContentBar, "HeroSection": HeroSection, "IntegrationSection": IntegrationSection, "WhatSection": WhatSection, "WhySection": WhySection}
     rendered = str(__Template().render("__nexy__//src/routes/index.html", context))
     styles = """"""
     return rendered + styles
