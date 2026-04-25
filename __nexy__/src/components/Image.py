19b9a66a8a1c04f13779e79f8090753f4702dd89
hopcy-forcy <hopcy.forcy@gmail.com>
2026-04-25 14:04:29 +0100

    feat: add Image component with support for src, srcset, className, and alt attributes
    - Created Image.html template for rendering images.
    - Implemented Image.py component to handle image rendering logic, including Vercel path adjustments.
    - Added client and server port configuration files.
    - Introduced Image.nexy file for component properties and rendering.

---
+++ __nexy__/src/components/Image.py
@@ -0,0 +1,17 @@
+from typing import *
+from fastapi import *
+from pathlib import Path as __Path
+from nexy import Template as __Template , Import as __Import
+from jinja2 import Template as __JinjaTemplate
+NexyElement = Union[callable, __JinjaTemplate]
+
+def Image(src: str = None, srcset: str = None, className: str = None, alt: str = None) -> str:
+        is_vercel = os.environ.get('VERCEL') == '1'
+    if is_vercel and src.startswith('/public/'):
+        src = src.replace('/public/', '/')
+        srcset = srcset.replace('/public/', '/') if srcset else None
+    
+    context = {"alt": alt, "className": className, "is_vercel": is_vercel, "src": src, "srcset": srcset}
+    rendered = str(__Template().render("__nexy__//src/components/Image.html", context))
+    styles = """"""
+    return rendered + styles
