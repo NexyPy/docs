d06c8cc2368dbcc9633ca9ca67ffce803bc87aa9
hopcy-forcy <hopcy.forcy@gmail.com>
2026-04-27 14:28:06 +0100

    feat: add search button component and update form and search HTML templates
    - Added a new search button component in `search-6TwtVYQE.js`.
    - Removed unused default components for Counter, Card, Form, Theme, and Vercel.
    - Updated HTML templates for Form and Search components to enhance styling and accessibility.
    - Introduced a new production file `nexy.prod`.
    - Updated various binary files in the `__pycache__` directories across components and routes.

--- __nexy__/client/entries/src/components/Counter.Default.tsx
+++
@@ -1,6 +0,0 @@
-import { hydrateRoot } from 'react-dom/client'
-import React from 'react'
-import Component from '../../../../../src/components/Counter'
-
-const el = document.getElementById('Counter.Default-root')
-if (el) hydrateRoot(el, React.createElement(Component))
--- __nexy__/client/entries/src/components/theme.Default.tsx
+++
@@ -1,6 +0,0 @@
-import { hydrateRoot } from 'react-dom/client'
-import React from 'react'
-import Component from '../../../../../src/components/theme'
-
-const el = document.getElementById('theme.Default-root')
-if (el) hydrateRoot(el, React.createElement(Component))
